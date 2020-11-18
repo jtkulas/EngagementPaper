55861f1296ef771195c645808aecf4e6a3512e8c
casosorio <64444956+casosorio@users.noreply.github.com>
2020-11-18 15:03:49 -0500

    Spliting Data by Conditions, and getting data ready for analysis.

--- Cleaning the Data.R
+++ Cleaning the Data.R
@@ -36,9 +36,12 @@ df2_Num<-df_Num[-c(2:3),]
 df2_Num<-df2_Num[-c(1:17)]
 
 
+
+# Hours 
 y<-df2_Num[146]
 y<-na.omit(y)
 y<-y[-c(1),]
+
 
 library(stringr)
 library(tidyverse)
@@ -59,3 +62,75 @@ ggplot(hours, aes(x = Hours)) +
   geom_vline(aes(xintercept=mean(hours, na.rm=T)),   # Ignore NA values for mean
              color="blue", linetype="dotted", size=1) +
   labs(x="Average number of hours worked (per week)")
+
+
+
+# Split the Data by Condition
+
+library(psych)
+
+Condition1<-df2_Num[,1:36]
+Condition2<-df2_Num[,37:72]
+Condition3<-df2_Num[,73:108]
+Condition4<-df2_Num[,109:144]
+
+Con1<-Condition1[-c(1),]
+Con2<-Condition2[-c(1),]
+Con3<-Condition3[-c(1),]
+Con4<-Condition4[-c(1),]
+
+
+# Changing to numeric
+
+Con1<-as.data.frame(apply(Con1,2, as.numeric))
+Con2<-as.data.frame(apply(Con2,2, as.numeric))
+Con3<-as.data.frame(apply(Con3,2, as.numeric))
+Con4<-as.data.frame(apply(Con4,2, as.numeric))
+
+# Spliting Conditions Further:
+  # Condition 1: affective, behavioral, cognitive
+    # Affective
+      
+      Affective_Absorption<-Con1[,1:4]
+      Affective_Vigor<-Con1[,5:8]
+      Affective_Dedication<-Con1[,9:12]
+    
+    # Behavioral
+      
+      Behavioral_Absorption<-Con1[,13:16]
+      Behavioral_Vigor<-Con1[,17:20]
+      Behavioral_Dedication<-Con1[,21:24]
+      
+    # Cognitive 
+      
+      Cognitive_Vigor<-Con1[,25:28]
+      Cognitive_Dedication<-Con1[,29:32]
+      Cognitive_Absorption<-Con1[,33:36]
+
+  # Condition 2: Absorption, Vigor, Dedication
+      # Absorption
+      
+      Absorption_Cognitive<-Con1[,1:4]
+      Absorption_Behavioral<-Con1[,5:8]
+      Absorption_Affective<-Con1[,9:12]
+      
+      # Vigor
+      
+      Vigor_Cognitive<-Con1[,13:16]
+      Vigor_Affective<-Con1[,17:20]
+      Vigor_Behavioral<-Con1[,21:24]
+      
+      # Dedication
+      
+      Dedication_Cognitive<-Con1[,25:28]
+      Dedication_Affective<-Con1[,29:32]
+      Dedication_Behavioral<-Con1[,33:36]
+
+
+
+#Items Stats
+alpha(Con1,na.rm = TRUE, check.keys = TRUE)
+alpha(Affective_Absorption,na.rm = TRUE,check.keys = TRUE)
+
+warnings()
+
