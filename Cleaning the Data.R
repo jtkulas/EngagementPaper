7686d240415e3dc845e9aa6f8d4cd379d1ecd5ef
casosorio <64444956+casosorio@users.noreply.github.com>
2020-11-29 22:11:43 -0500

    Working on Item Analysis

--- Cleaning the Data.R
+++ Cleaning the Data.R
@@ -33,7 +33,7 @@ x <- df_Num[2,]
 colnames(df_Num) <- x
 
 df2_Num<-df_Num[-c(2:3),]
-df2_Num<-df2_Num[-c(1:17)]
+df2_Num<-df2_Num[-c(1),-c(1:17)]
 
 
 
@@ -45,6 +45,9 @@ y<-y[-c(1),]
 
 library(stringr)
 library(tidyverse)
+library(lavaan)
+library(sem)
+library(semPlot)
 
 y<-gsub("\\D","",y,ignore.case = TRUE, fixed =FALSE)
 
@@ -69,15 +72,11 @@ ggplot(hours, aes(x = Hours)) +
 
 library(psych)
 
-Condition1<-df2_Num[,1:36]
-Condition2<-df2_Num[,37:72]
-Condition3<-df2_Num[,73:108]
-Condition4<-df2_Num[,109:144]
-
-Con1<-Condition1[-c(1),]
-Con2<-Condition2[-c(1),]
-Con3<-Condition3[-c(1),]
-Con4<-Condition4[-c(1),]
+Con1<-df2_Num[,1:36]
+Con1<-na.omit(Con1)
+Con2<-df2_Num[,37:72]
+Con3<-df2_Num[,73:108]
+Con4<-df2_Num[,109:144]
 
 
 # Changing to numeric
@@ -132,5 +131,154 @@ Con4<-as.data.frame(apply(Con4,2, as.numeric))
 alpha(Con1,na.rm = TRUE, check.keys = TRUE)
 alpha(Affective_Absorption,na.rm = TRUE,check.keys = TRUE)
 
-warnings()
+
+Con1<-Con1%>%rename(C1.1a=`C1.1 - I enjoy thinking about work even when Iâ€™m not at work.`,
+                    C1.1b=`C1.1 - Most days, I feel happiest when the workday is soon to be complete.`,
+                    C1.1c=`C1.1 - I am happiest when I am immersed in a project.`,
+                    C1.1d=`C1.1 - I love starting my workday.`,
+                    
+                    C1.2a=`C1.2 - I enjoy spending time completing my job tasks.`,
+                    C1.2b=`C1.2 - Most days I feel enthusiastic about starting my work day.`,
+                    C1.2c=`C1.2 - I feel motivated to go beyond what is asked of me.`,
+                    C1.2d=`C1.2 - This job drains my energy.`,
+                    
+                    C1.3a=`C1.3 - I am proud to be a member of this organization.`,
+                    C1.3b=`C1.3 - I feel supported by my supervisor when I fail at a task.`,
+                    C1.3c=`C1.3 - I feel proud of my accomplishments within this organization.`,
+                    C1.3d=`C1.3 - My job makes me feel like Iâ€™m part of something meaningful.`,
+                    
+                    C1.4a=`C1.4 - I devote more time than is expected of me.`,
+                    C1.4b=`C1.4 - I have to be reminded to take breaks while Iâ€™m at work.`,
+                    C1.4c=`C1.4 - I never miss a work deadline.`,
+                    C1.4d=`C1.4 - I never allow distractions to interfere with my work.`,
+                    
+                    C1.5a=`C1.5 - When work is slow I find ways to be productive.`,
+                    C1.5b=`C1.5 - I express enthusiasm for my job while at work.`,
+                    C1.5c=`C1.5 - I try my best to perform well at work.`,
+                    C1.5d=`C1.5 - If I notice my energy level is low, I take corrective steps to re-energize.`,
+                    
+                    C1.6a=`C1.6 - I make valued contributions to the organization.`,
+                    C1.6b=`C1.6 - I embrace challenging situations at work.`,
+                    C1.6c=`C1.6 - I speak positively about this organization to others.`,
+                    C1.6d=`C1.6 - This organization provides the resources necessary for me to successfully perform my job.`,
+                    
+                    C1.7a=`C1.7 - I devote my full attention to my work tasks throughout the day.`,
+                    C1.7b=`C1.7 - Thinking about work saps my energy.`,
+                    C1.7c=`C1.7 - I would rather direct my focus toward a work task than a personal task.`,
+                    C1.7d=`C1.7 - Iâ€™m able to maintain good levels of energy throughout the workday.`,
+                    
+                    C1.8a=`C1.8 - I plan my future with this company.`,
+                    C1.8b=`C1.8 - I believe this company cares about my career goals.`,
+                    C1.8c=`C1.8 - I often think about finding another job.`,
+                    C1.8d=`C1.8 - This organization challenges me to work at my full potential.`,
+                    
+                    C1.9a=`C1.9 - Iâ€™m able to concentrate on my work without distractions.`,
+                    C1.9b=`C1.9 - I have a hard time detaching mentally from my work.`,
+                    C1.9c=`C1.9 - Time passes quickly while Iâ€™m working.`,
+                    C1.9d=`C1.9 - I find it difficult to mentally disconnect from work.`
+                    )
+
+
+
+
+
+# CFA Models that work
+
+Model1.1<-'
+Affective =~ C1.1a + C1.1b + C1.1c + C1.1d + C1.2a + C1.2b + C1.2c + C1.2d + C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral =~ C1.4a + C1.4b + C1.4c + C1.4d + C1.5a + C1.5b + C1.5c + C1.5d + C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive =~ C1.9a + C1.9b + C1.9c + C1.9d + C1.7a + C1.7b + C1.7c + C1.7d + C1.8a + C1.8b + C1.8c + C1.8d
+'
+Fit1.1<-lavaan::cfa(Model1.1, data = Con1)
+semPlot::semPaths(Fit1.1,whatLabels = "std", layout = "tree")
+lavaan::fitMeasures(Fit1.1)
+summary(Fit1.1, fit.measure=TRUE)
+
+
+
+Model1.2<-'
+Affective =~ C1.1a + C1.1b + C1.1c + C1.1d + C1.2a + C1.2b + C1.2c + C1.2d + C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral =~ C1.4a + C1.4b + C1.4c + C1.4d + C1.5a + C1.5b + C1.5c + C1.5d + C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive =~ C1.9a + C1.9b + C1.9c + C1.9d + C1.7a + C1.7b + C1.7c + C1.7d + C1.8a + C1.8b + C1.8c + C1.8d
+global =~ Affective + Behavioral + Cognitive
+'
+Fit1.2<-lavaan::cfa(Model1.2, data = Con1)
+semPlot::semPaths(Fit1.2,whatLabels = "std", layout = "tree")
+lavaan::fitMeasures(Fit1.2)
+summary(Fit1.2, fit.measure=TRUE)
+
+
+ModelBifactor<-'
+Affective =~ C1.1a + C1.1b + C1.1c + C1.1d + C1.2a + C1.2b + C1.2c + C1.2d + C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral =~ C1.4a + C1.4b + C1.4c + C1.4d + C1.5a + C1.5b + C1.5c + C1.5d + C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive =~ C1.9a + C1.9b + C1.9c + C1.9d + C1.7a + C1.7b + C1.7c + C1.7d + C1.8a + C1.8b + C1.8c + C1.8d
+global =~ C1.1a + C1.1b + C1.1c + C1.1d + C1.2a + C1.2b + C1.2c + C1.2d + C1.3a + C1.3b + C1.3c + C1.3d + C1.4a + C1.4b + C1.4c + C1.4d + C1.5a + C1.5b + C1.5c + C1.5d + C1.6a + C1.6b + C1.6c + C1.6d + C1.9a + C1.9b + C1.9c + C1.9d + C1.7a + C1.7b + C1.7c + C1.7d + C1.8a + C1.8b + C1.8c + C1.8d
+'
+
+FitBifactor<-lavaan::cfa(ModelBifactor, data = Con1, orthogonal=TRUE, std.lv=TRUE)
+semPlot::semPaths(FitBifactor,whatLabels = "std", layout = "tree")
+lavaan::fitMeasures(FitBifactor)
+summary(FitBifactor, fit.measure=TRUE)
+
+
+
+# CFA that do not work
+
+
+first.model = '
+Affective.Absorption =~ C1.1a + C1.1b + C1.1c + C1.1d
+Affective.Vigor =~ C1.2a + C1.2b + C1.2c + C1.2d
+Affective.Dedication =~ C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral.Absorption =~ C1.4a + C1.4b + C1.4c + C1.4d
+Behavioral.Vigor =~ C1.5a + C1.5b + C1.5c + C1.5d
+Behavioral.Dedication =~ C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive.Absorption =~ C1.9a + C1.9b + C1.9c + C1.9d
+Cognitive.Vigor =~ C1.7a + C1.7b + C1.7c + C1.7d
+Cogntive.Dedication =~ C1.8a + C1.8b + C1.8c + C1.8d
+'
+
+
+second.model ='
+Affective.Absorption =~ C1.1a + C1.1b + C1.1c + C1.1d
+Affective.Vigor =~ C1.2a + C1.2b + C1.2c + C1.2d
+Affective.Dedication =~ C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral.Absorption =~ C1.4a + C1.4b + C1.4c + C1.4d
+Behavioral.Vigor =~ C1.5a + C1.5b + C1.5c + C1.5d
+Behavioral.Dedication =~ C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive.Absorption =~ C1.9a + C1.9b + C1.9c + C1.9d
+Cognitive.Vigor =~ C1.7a + C1.7b + C1.7c + C1.7d
+Cogntive.Dedication =~ C1.8a + C1.8b + C1.8c + C1.8d
+generalfactor =~ Affective.Absorption + Affective.Vigor + Affective.Dedication + Behavioral.Absorption + Behavioral.Vigor + Behavioral.Dedication + Cognitive.Absorption + Cognitive.Vigor + Cogntive.Dedication
+'
+
+alternative.model ='
+Affective.Absorption =~ C1.1a + C1.1b + C1.1c + C1.1d
+Affective.Vigor =~ C1.2a + C1.2b + C1.2c + C1.2d
+Affective.Dedication =~ C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral.Absorption =~ C1.4a + C1.4b + C1.4c + C1.4d
+Behavioral.Vigor =~ C1.5a + C1.5b + C1.5c + C1.5d
+Behavioral.Dedication =~ C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive.Absorption =~ C1.9a + C1.9b + C1.9c + C1.9d
+Cognitive.Vigor =~ C1.7a + C1.7b + C1.7c + C1.7d
+Cogntive.Dedication =~ C1.8a + C1.8b + C1.8c + C1.8d
+generalfactor =~ NA*Affective.Absorption + Affective.Vigor + Affective.Dedication + Behavioral.Absorption + Behavioral.Vigor + Behavioral.Dedication + Cognitive.Absorption + Cognitive.Vigor + Cogntive.Dedication
+generalfactor ~~ 1*generalfactor
+'
+
+
+bifactor.model='
+Affective.Absorption =~ C1.1a + C1.1b + C1.1c + C1.1d
+Affective.Vigor =~ C1.2a + C1.2b + C1.2c + C1.2d
+Affective.Dedication =~ C1.3a + C1.3b + C1.3c + C1.3d
+Behavioral.Absorption =~ C1.4a + C1.4b + C1.4c + C1.4d
+Behavioral.Vigor =~ C1.5a + C1.5b + C1.5c + C1.5d
+Behavioral.Dedication =~ C1.6a + C1.6b + C1.6c + C1.6d
+Cognitive.Absorption =~ C1.9a + C1.9b + C1.9c + C1.9d
+Cognitive.Vigor =~ C1.7a + C1.7b + C1.7c + C1.7d
+Cogntive.Dedication =~ C1.8a + C1.8b + C1.8c + C1.8d
+generalfactor =~ C1.1a + C1.1b + C1.1c + C1.1d + C1.2a + C1.2b + C1.2c + C1.2d + C1.3a + C1.3b + C1.3c + C1.3d + C1.4a + C1.4b + C1.4c + C1.4d + C1.5a + C1.5b + C1.5c + C1.5d + C1.6a + C1.6b + C1.6c + C1.6d + C1.9a + C1.9b + C1.9c + C1.9d + C1.7a + C1.7b + C1.7c + C1.7d + C1.8a + C1.8b + C1.8c + C1.8d
+'
+
+
+
 
