b055eea9d47b7b5121c425ba8b45fdeb7f9c2eff
Renata Garcia Prieto Palacios Roji <garciaprier1@admsu.montclair.edu>
2020-12-08 13:06:55 -0500

    changes

--- renataclean.R
+++ renataclean.R
@@ -1,26 +1,64 @@
 # Importing Data 
 
-temp <- read.csv("qualtrics_pilot_data.csv", header=FALSE)
+temp <- read.csv("qualtrics_pilot_data.csv", header=TRUE, na.strings="")
 library(tidyverse)
 glimpse(temp)
 
-data <- as_tibble(temp)
+# Libraries 
+
+library(stringr)
+library(tidyverse)
+library(lavaan)
+library(sem)
+library(semPlot)
+library(ggplot2)
+
+data <- temp
 glimpse(data)
 library(dplyr)
 
-# Remove rows 1 and 3
-test <- data[-c(1,3),]
+# Remove columns and rows 1 and 3
+data2 <- data[-c(1:3),-c(1:17)]
+test <- data2[-c(1,3),]
 
-#Remove columns
-test$V10 <- NULL
-test$V11 <- NULL
-test$V12 <- NULL
-test$V13 <- NULL
- 
-# Adding a column
 
-library(tibble)
-add_column(test, .after = "V17") # we can change this to the name of the column
+# Caseys code: 
+
+# Split the Data by Condition- Option 2
+
+library(psych)
+
+Con1<-test %>% select(C1.1_1:C1.9_4)
+Con1<-na.omit(Con1)
+Con2<-test %>% select(C2.1_1:C2.9_4)
+Con2<-na.omit(Con2)
+Con3<-test %>% select(C3.1_1:C3.3_12)
+Con3<-na.omit(Con3)
+Con4<-test %>% select(C4.1_1:C4.3_12)
+Con4<-na.omit(Con4)
+
+
+# Changing to numeric
+
+Con1<-as.data.frame(apply(Con1,2, as.numeric))
+Con2<-as.data.frame(apply(Con2,2, as.numeric))
+Con3<-as.data.frame(apply(Con3,2, as.numeric))
+Con4<-as.data.frame(apply(Con4,2, as.numeric))
+
+
+# Subsetting to 4 conditions
+
+Condition1 <- test["C1.1_1":"C1.9_4"]
+Condition2 <- test[, c(51:85)]
+Condition3 <- test[, c(86:121)]
+Condition4 <- test[, c(122:157)]
+Demographic <- test[, c(159:163)]
+
+Condition1 <- test[, c(15:50)]
+Condition2 <- test[, c(51:85)]
+Condition3 <- test[, c(86:121)]
+Condition4 <- test[, c(122:157)]
+Demographic <- test[, c(159:163)]
 
 # ID column
 
@@ -28,13 +66,7 @@ test$id <- seq.int(nrow(test))
 col_num <- grep("id", names(test))
 test <- test[, c(col_num, (1:ncol(test))[-col_num])]
 
-# Subsetting to 4 conditions
 
-Condition1 <- test[, c(15:50)]
-Condition2 <- test[, c(51:85)]
-Condition3 <- test[, c(86:121)]
-Condition4 <- test[, c(122:157)]
-Demographic <- test[, c(159:163)]
 
 
 # Blank values to filter out responses
@@ -47,12 +79,5 @@ Condition4.2 <- Condition4[!apply(Condition4 == "", 1, all), ]
 
 # Filling in New column 
 
-
-# Renaming Columns 
-
-data2<-test %>%
-   rename(Start.Date = V1, End.Date = V2, Status = V3, IPAddress = V4, Progress = V5, SecDuration = V6, 
-        Finished = V7, RecordedDate = V8, ReponseID = V9, Latitude = V14, Longitude = V15, DistChannel = V16, 
-        Language = V17, Item1 = V18, Item2 = V19)
 
 
