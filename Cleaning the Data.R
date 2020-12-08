1aa481c047561d2a11d5e2ca517a2291c2309be7
casosorio <64444956+casosorio@users.noreply.github.com>
2020-12-08 15:26:07 -0500

    Creating condition column-option 1 is almost complete, it just one little bug I cannot fix. But I am trying to figure it out.

--- Cleaning the Data.R
+++ Cleaning the Data.R
@@ -31,7 +31,7 @@ df_Num <- read.csv("Qualtrics_pilot_data.csv", header=TRUE,na.strings="")
 
 df2_Num<-df_Num[-c(1:3),]
 
-df_Num<-df2_Num%>%select(C1.1_1:Q29)
+df2_Num<-df2_Num%>%select(C1.1_1:Q29)
 
 
 
@@ -74,23 +74,85 @@ df2_Num$C4.2_8<-car::recode(df2_Num$C4.2_8,'1=6; 2=5; 3=4; 4=3; 5=2; 6=1; 7=7')
 
 # Creating Condition Column - Option 1
 
-Condition1<-as.data.frame(ifelse(df2_Num$C1.1_1!='NA',"Condition 1"))
-Condition1<-Condition1%>%rename(Condition_1=`ifelse(df2_Num$C1.1_1 != "NA", "Condition 1")`)
+Condition1<-as.data.frame(ifelse(df2_Num%>%select(C1.1_1:C1.9_4)!='NA',"Condition 1"))
+
+Condition1<-Condition1%>%unite(Condition, C1.1_1, C1.1_2, C1.1_3 , C1.1_4 , C1.2_1 , C1.2_2 , C1.2_3 , 
+                                      C1.2_4 , C1.3_1 , C1.3_2 , C1.3_3 , C1.3_4,
+                                      C1.4_1 , C1.4_2 , C1.4_3 , C1.4_4 , 
+                                      C1.5_1 , C1.5_2 , C1.5_3 , C1.5_4 , 
+                                      C1.6_1 , C1.6_2 , C1.6_3 , C1.6_4,
+                                      C1.7_1 , C1.7_2 , C1.7_3 , C1.7_4 , 
+                                      C1.8_1 , C1.8_2 , C1.8_3 , C1.8_4,
+                                      C1.9_1 , C1.9_2 , C1.9_3 , C1.9_4, na.rm=TRUE
+)
+
+Condition1<-as.data.frame(substr(Condition1$Condition,1,11))
+Condition1<-Condition1%>%rename(Condition_1=`substr(Condition1$Condition, 1, 11)`)
+
+#condition2
+
+Condition2<-as.data.frame(ifelse(df2_Num%>%select(C2.1_1:C2.9_4)!='NA',"Condition 2"))
+
+Condition2<-Condition2%>%unite(Condition, C2.1_1, C2.1_2, C2.1_3 , C2.1_4 , C2.2_1 , C2.2_2 , C2.2_3 , 
+                               C2.2_4 , C2.3_1 , C2.3_2 , C2.3_3 , C2.3_4,
+                               C2.4_1 , C2.4_2 , C2.4_3 , C2.4_4 , 
+                               C2.5_1 , C2.5_2 , C2.5_3 , C2.5_4 , 
+                               C2.6_1 , C2.6_2 , C2.6_3 , C2.6_4,
+                               C2.7_1 , C2.7_2 , C2.7_3 , C2.7_4 , 
+                               C2.8_1 , C2.8_2 , C2.8_3 , C2.8_4,
+                               C2.9_1 , C2.9_2 , C2.9_3 , C2.9_4, na.rm=TRUE
+)
+
+Condition2<-as.data.frame(substr(Condition2$Condition,1,11))
+Condition2<-Condition2%>%rename(Condition_2=`substr(Condition2$Condition, 1, 11)`)
+
+#Condition 3
+Condition3<-as.data.frame(ifelse(df2_Num%>%select(C3.1_1:C3.3_12)!='NA',"Condition 3"))
+
+Condition3<-Condition3%>%unite(Condition, C3.1_1, C3.1_2, C3.1_3 , C3.1_4 , C3.1_5 , C3.1_6 , C3.1_7 , 
+                               C3.1_8 , C3.1_9 , C3.1_10 , C3.1_11 , C3.1_12,
+                               C3.2_1 , C3.2_2 , C3.2_3 , C3.2_4 , 
+                               C3.2_5 , C3.2_6 , C3.2_7 , C3.2_8 , 
+                               C3.2_9 , C3.2_10 , C3.2_11 , C3.2_12,
+                               C3.3_1 , C3.3_2 , C3.3_3 , C3.3_4 , 
+                               C3.3_5 , C3.3_6 , C3.3_7 , C3.3_8,
+                               C3.3_9 , C3.3_10 , C3.3_11 , C3.3_12, na.rm=TRUE
+)
+
+Condition3<-as.data.frame(substr(Condition3$Condition,1,11))
+Condition3<-Condition3%>%rename(Condition_3=`substr(Condition3$Condition, 1, 11)`)
+
+#Condition 4
 
 
-Condition2<-as.data.frame(ifelse(df2_Num$C2.1_1!='NA',"Condition 2"))
-Condition2<-Condition2%>%rename(Condition_2=`ifelse(df2_Num$C2.1_1 != "NA", "Condition 2")`)
+Condition4<-as.data.frame(ifelse(df2_Num%>%select(C4.1_1:C4.3_12)!='NA',"Condition 4"))
 
-Condition3<-as.data.frame(ifelse(df2_Num$C3.1_1!='NA',"Condition 3"))
-Condition3<-Condition3%>%rename(Condition_3=`ifelse(df2_Num$C3.1_1 != "NA", "Condition 3")`)
+Condition4<-Condition4%>%unite(Condition, C4.1_1, C4.1_2, C4.1_3 , C4.1_4 , C4.1_5 , C4.1_6 , C4.1_7 , 
+                               C4.1_8 , C4.1_9 , C4.1_10 , C4.1_11 , C4.1_12,
+                               C4.2_1 , C4.2_2 , C4.2_3 , C4.2_4 , 
+                               C4.2_5 , C4.2_6 , C4.2_7 , C4.2_8 , 
+                               C4.2_9 , C4.2_10 , C4.2_11 , C4.2_12,
+                               C4.3_1 , C4.3_2 , C4.3_3 , C4.3_4 , 
+                               C4.3_5 , C4.3_6 , C4.3_7 , C4.3_8,
+                               C4.3_9 , C4.3_10 , C4.3_11 , C4.3_12, na.rm=TRUE
+)
 
-Condition4<-as.data.frame(ifelse(df2_Num$C4.1_1!='NA',"Condition 4"))
-Condition4<-Condition4%>%rename(Condition_4=`ifelse(df2_Num$C4.1_1 != "NA", "Condition 4")`)
+Condition4<-as.data.frame(substr(Condition4$Condition,1,11))
+Condition4<-Condition4%>%rename(Condition_4=`substr(Condition4$Condition, 1, 11)`)
+
+
+
+
+
+#Merging 
+
 
 Condition<-cbind(Condition1, Condition2, Condition3, Condition4)
 
 Condition<-Condition%>%unite(Conditions, Condition_1, Condition_2, Condition_3, Condition_4,
                              na.rm=TRUE)
+
+Condition$Conditions<-trimws(Condition$Conditions)
 
 df3_Num<-cbind(Condition,df2_Num)
 
@@ -119,6 +181,7 @@ Con4<-as.data.frame(apply(Con4,2, as.numeric))
 
 
 # Creating the new column
+
 Con1<-cbind(Condition="Condition 1",Con1)
 Con2<-cbind(Condition="Condition 2",Con2)
 Con3<-cbind(Condition="Condition 3",Con3)
@@ -127,6 +190,10 @@ Con4<-cbind(Condition="Condition 4",Con4)
 data<-merge(Con1,Con2, by="Condition",all=T)
 data2<-merge(Con3,Con4, by="Condition", all = T)
 data3<-merge(data,data2, by="Condition", all=T)
+
+
+
+
 
 # Demographics
 Demographics<-df2_Num[,145:148]
