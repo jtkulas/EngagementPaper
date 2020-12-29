2ea426b63516a56172ee90b5eb6f71fd2bb1d2bc
casosorio <64444956+casosorio@users.noreply.github.com>
2020-12-29 13:04:34 -0500

    Problems with coverging the models

--- CFA Anlysis.R
+++ CFA Anlysis.R
@@ -120,10 +120,15 @@ Bifactor_Model<-'
 Absorption=~Item_1+Item_2+Item_3+Item_4+Item_5+Item_6+Item_7+Item_8+Item_9+Item_10+Item_11+Item_12
 Vigor=~Item_13+Item_14+Item_15+Item_16+Item_17+Item_18+Item_19+Item_20+Item_21+Item_22+Item_23+Item_24
 Dedication=~Item_25+Item_26+Item_27+Item_28+Item_29+Item_30+Item_31+Item_32+Item_33+Item_34+Item_35+Item_36
-global=~Item_1+Item_2+Item_3+Item_4+Item_5+Item_6+Item_7+Item_8+Item_9+Item_10+Item_11+Item_12+Item_13+Item_14+Item_15+Item_16+Item_17+Item_18+Item_19+Item_20+Item_21+Item_22+Item_23+Item_24+Item_25+Item_26+Item_27+Item_28+Item_29+Item_30+Item_31+Item_32+Item_33+Item_34+Item_35+Item_36
+Cognitive=~Item_1+Item_2+Item_3+Item_4+Item_13+Item_14+Item_15+Item_16+Item_25+Item_26+Item_27+Item_28
+Affective=~Item_5+Item_6+Item_7+Item_8+Item_17+Item_18+Item_19+Item_20+Item_29+Item_30+Item_31+Item_32
+Behavioral=~Item_9+Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_24+Item_33+Item_34+Item_35+Item_36
+global=~NA*Absorption+Vigor+Dedication
+global2=~Cognitive+Affective+Behavioral
+global~~1*global
 '
 
-Fit_Bifactor<-lavaan::cfa(Bifactor_Model,data = CFAdata, orthogonal=TRUE)
+Fit_Bifactor<-lavaan::cfa(Bifactor_Model,data = CFAdata)
 semPlot::semPaths(Fit_Bifactor,whatLabels = "std",layout="tree")
 lavaan::fitmeasures(Fit_Bifactor)
 summary(Fit_Bifactor,fit.measure=TRUE)
