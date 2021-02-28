15cc50665f83c217f425c17fa7d59b4afde78d96
Your <your@email.com>
2021-02-28 14:15:53 -0500

    R Drops

---
+++ R Drops.R
@@ -0,0 +1,14 @@
+# R Drops
+
+aff.abs <- psych::alpha(together[2:5])
+aff.vig <- psych::alpha(together[6:9])
+aff.ded <- psych::alpha(together[10:13])
+
+beh.abs <- psych::alpha(together[14:17])
+beh.vig <- psych::alpha(together[18:21])
+beh.ded <- psych::alpha(together[22:25])
+
+cog.abs <- psych::alpha(together[34:37])
+cog.vig <- psych::alpha(together[26:29])
+cog.ded <- psych::alpha(together[30:33])
+
