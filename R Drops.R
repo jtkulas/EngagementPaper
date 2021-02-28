e4cee70a0d3a4b12785d98fbbe9b17ea5593cfff
Your <your@email.com>
2021-02-28 15:25:17 -0500

    One item removed

--- R Drops.R
+++ R Drops.R
@@ -1,14 +1,21 @@
 # R Drops
 
-aff.abs <- psych::alpha(together[2:5])
-aff.vig <- psych::alpha(together[6:9])
-aff.ded <- psych::alpha(together[10:13])
+aff.abs <- psych::alpha(together[,c(2,3,5)])
+aff.vig <- psych::alpha(together[6:8])
+aff.ded <- psych::alpha(together[,c(10,12,13)])
 
-beh.abs <- psych::alpha(together[14:17])
-beh.vig <- psych::alpha(together[18:21])
+beh.abs <- psych::alpha(together[,c(14,15,17)])
+beh.vig <- psych::alpha(together[,c(18,19,21)])
 beh.ded <- psych::alpha(together[22:25])
 
 cog.abs <- psych::alpha(together[34:37])
 cog.vig <- psych::alpha(together[26:29])
 cog.ded <- psych::alpha(together[30:33])
 
+
+aff.abs
+aff.vig
+aff.ded
+
+beh.abs
+beh.vig
