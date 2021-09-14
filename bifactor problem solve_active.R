6b0febe734d2f598f646dd17670f01f6262b728b
John-o Kulas <kulasj@montclair.edu>
2021-09-14 11:32:54 -0400

    engagement convergent measure

--- bifactor problem solve_active.R
+++ bifactor problem solve_active.R
@@ -76,13 +76,15 @@ Vigor ~~ 0*Cognitive
 Dedication ~~ 0*Affective
 Dedication ~~ 0*Behavioral
 Dedication ~~ 0*Cognitive
+Item_16 ~~ 1*Item_16         ## was (small) Heywood case
 '
-#Dedication ~~ 0*Vigor      
-#Dedication ~~ 0*Absorption
-#Vigor ~~ 0*Absorption
-#Affective ~~ 0*Behavioral
-#Affective ~~ 0*Cognitive
-#Behavioral ~~ 0*Cognitive
+
+# Dedication ~~ 0*Vigor      
+# Dedication ~~ 0*Absorption
+# Vigor ~~ 0*Absorption
+# Affective ~~ 0*Behavioral
+# Affective ~~ 0*Cognitive
+# Behavioral ~~ 0*Cognitive
 #'
 
 Fit.mod1 <- lavaan::cfa(modified1, data = CFAdata) 
@@ -92,15 +94,39 @@ semPlot::semPaths(Fit.mod1, bifactor = c("Cognitive", "Affective", "Behavioral")
 
 modindices(Fit.mod1, sort = TRUE, maximum.number = 25)
 
-summary(Fit.mod1)    ## 340.76
+summary(Fit.mod1, standardized=TRUE)    ## 340.76
 
 ## write.csv(psych::describe(CFAdata), "descriptives.csv")
 
-library(tidyverse)
+#library(tidyverse)
 
-write.csv(CFAdata %>% 
-  select(Item_3,Item_2,Item_5,Item_7,Item_8,Item_10,Item_15,Item_16,Item_17,Item_19,
-         Item_21,Item_22,Item_24) %>% 
-  cor(use = "complete.obs"), "smallcor.csv")
+#write.csv(CFAdata %>% 
+#  select(Item_1,Item_3,Item_13,Item_16,Item_26,Item_28,Item_5,Item_8,Item_17,Item_18,Item_29,Item_32,Item_9,Item_10,Item_22,Item_23,Item_33,Item_34,Item_29,Item_30, Item_31, Item_32) %>% 
+#  cor(use = "complete.obs"), "smallcor.csv")
 
+## Casey's below
+
+#Bifactor_Model<-'
+#Cognitive=~Item_1+Item_3+Item_13+Item_16+Item_26+Item_28
+#Affective=~Item_5+Item_8+Item_17+Item_18+Item_30+Item_31
+#Behavioral=~Item_9+Item_10+Item_22+Item_23+Item_33+Item_34
+#Absorption=~Item_1+Item_3+Item_5++Item_8+Item_9+Item_10
+#Vigor=~Item_13+Item_16+Item_17+Item_18+Item_22+Item_23
+#Dedication=~Item_26+Item_28+Item_30+Item_31+Item_33+Item_34
+#Absorption ~~ 0*Affective
+#Absorption ~~ 0*Behavioral
+#Absorption ~~ 0*Cognitive
+#Vigor ~~ 0*Affective
+#Vigor ~~ 0*Behavioral
+#Vigor ~~ 0*Cognitive
+#Dedication ~~ 0*Affective
+#Dedication ~~ 0*Behavioral
+#Dedication ~~ 0*Cognitive
+#'
+
+
+#casey.mod1 <- lavaan::cfa(Bifactor_Model, data = CFAdata) 
+
+#semPlot::semPaths(casey.mod1, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
+#                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0, edge.label.cex = 1.5, pastel=TRUE) 
 
