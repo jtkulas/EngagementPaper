dfb3e241dd44623db6f0c35f5451feac26416bd6
John-o Kulas <kulasj@montclair.edu>
2021-10-15 17:22:06 -0400

    trying to salvage some Qualtrics responses

--- qualtrics_screen.R
+++ qualtrics_screen.R
@@ -1,6 +1,6 @@
 ## Cleaning Qualtrics construct validation data - 10/14/21
 
-newdata.att <- read.csv("Engagement+(Attitudinal)_October+12,+2021_08.02.csv")#[47:66]
+newdata.att <- read.csv("Engagement+(Attitudinal)_October+12,+2021_08.02.csv"); names <- newdata.att[1,c(1:92)] #[47:66]
 newdata.sub <- read.csv("Engagement+(Substantive)_October+12,+2021_08.01.csv")#[47:66]
 
 newdata.att <- newdata.att[-c(1:2),]                      ## peskies
@@ -28,9 +28,10 @@ descr::freq(newdata.sub$irv2)
 ## need filter for non-differentiating reverse-score people
 
 newdata.att$flag <- "use"; newdata.sub$flag <- "use"         ## need to do because subset on lines 44/45 can't != "Flagged"
+                                                             ## upped time to 300 seconds as data still looked bad with 200
 
-newdata.att$flag[newdata.att$careless_long > 12 | newdata.att$Duration..in.seconds. < 200 | newdata.att$irv == 0 | newdata.att$irv2 == 0]  <- "Flagged"
-newdata.sub$flag[newdata.sub$careless_long > 12 | newdata.sub$Duration..in.seconds. < 200 | newdata.sub$irv == 0 | newdata.sub$irv2 == 0]  <- "Flagged"
+newdata.att$flag[newdata.att$careless_long > 12 | newdata.att$Duration..in.seconds. < 300 | newdata.att$irv == 0 | newdata.att$irv2 == 0]  <- "Flagged"
+newdata.sub$flag[newdata.sub$careless_long > 12 | newdata.sub$Duration..in.seconds. < 300 | newdata.sub$irv == 0 | newdata.sub$irv2 == 0]  <- "Flagged"
 
 write.csv(newdata.att, "attitudeset.csv")
 write.csv(newdata.sub, "substantiveset.csv")
@@ -41,7 +42,36 @@ newdata.sub$flag <- as.factor(newdata.sub$flag)
 descr::freq(newdata.att$flag)
 descr::freq(newdata.sub$flag)
 
-useatt <- newdata.att[ which(newdata.att$flag == "use"), ]
-usesub <- newdata.sub[ which(newdata.sub$flag == "use"), ]
+useatt <- newdata.att[ which(newdata.att$flag == "use"), c(1:92)]
+usesub <- newdata.sub[ which(newdata.sub$flag == "use"), c(1:92)]
 
-psych::alpha(useatt[c(48:51)])
+useseb.sorted <- usesub[,c(1:47, 48:50,55:56,61:63,51:52,57:58,64:65,53:54,59:60,66:67,68:92)]
+names(useseb.sorted) <- names(useatt)           ##### focal engagement is 48:67
+
+qualtrics <- as.data.frame(rbind(useseb.sorted,useatt))
+
+########################################### Engagement
+
+qualtrics$C14 <- 7-qualtrics$C14
+
+cognition.alpha  <- psych::alpha(qualtrics[c(48:55)])
+affect.alpha     <- psych::alpha(qualtrics[c(56:61)])
+behavior.alpha   <- psych::alpha(qualtrics[c(62:67)])
+
+absorption.alpha <- psych::alpha(qualtrics[c(48:50,56:57,62:63)])
+vigor.alpha      <- psych::alpha(qualtrics[c(51:52,58:59,64:65)])
+dedication.alpha <- psych::alpha(qualtrics[c(53:55,60:61,66:67)])
+
+qualtrics$cognition <- rowMeans(qualtrics[c(48:55)], na.rm=TRUE)
+qualtrics$affect    <- rowMeans(qualtrics[c(56:61)], na.rm=TRUE)
+qualtrics$behavior  <- rowMeans(qualtrics[c(62:67)], na.rm=TRUE)
+
+qualtrics$absorption <- rowMeans(qualtrics[c(48:50,56:57,62:63)], na.rm=TRUE)
+qualtrics$vigor      <- rowMeans(qualtrics[c(51:52,58:59,64:65)], na.rm=TRUE)
+qualtrics$dedication <- rowMeans(qualtrics[c(53:55,60:61,66:67)], na.rm=TRUE)
+
+cor(qualtrics[93:98], use="complete.obs")
+
+########################################### Intent to quit
+
+intent.quit.alpha <- psych::alpha(qualtrics[c(80:83)])
