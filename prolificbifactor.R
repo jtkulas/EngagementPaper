89fee10832c213e5cebaa7fc54f26a00123a47a6
John-o Kulas <kulasj@montclair.edu>
2021-10-22 14:52:50 -0400

    prolific look

--- prolificbifactor.R
+++ prolificbifactor.R
@@ -40,7 +40,34 @@ data <- data[,c(1:404)]
 
 data$item387      <- 7- data$item387           ## only reflected engagement item
 
+names(data)[380] <- "Item_1"
+names(data)[381] <- "Item_3"
+names(data)[382] <- "Item_4"
+names(data)[383] <- "Item_5"
+names(data)[384] <- "Item_8"
+names(data)[385] <- "Item_10"
+names(data)[386] <- "Item_11"
+names(data)[387] <- "Item_14"
+names(data)[388] <- "Item_16"
+names(data)[389] <- "Item_17"
+names(data)[390] <- "Item_19"
+names(data)[391] <- "Item_21"
+names(data)[392] <- "Item_22"
+names(data)[393] <- "Item_25"
+names(data)[394] <- "Item_26"
+names(data)[395] <- "Item_28"
+names(data)[396] <- "Item_31"
+names(data)[397] <- "Item_32"
+names(data)[398] <- "Item_34"
+names(data)[399] <- "Item_35"
+
 psych::alpha(data[c(380:386)])
+psych::alpha(data[c(387:392)])
+psych::alpha(data[c(393:399)])
+psych::alpha(data[c(380:382, 387:388, 393:395)])
+psych::alpha(data[c(383:384, 389:390, 396:397)])
+psych::alpha(data[c(385:386, 391:392, 398:399)])
+
 
 data$absorption   <- rowMeans(data[c(380:386)], na.rm=TRUE)
 data$vigor        <- rowMeans(data[c(387:392)], na.rm=TRUE) 
@@ -49,3 +76,50 @@ data$dedication   <- rowMeans(data[c(393:399)], na.rm=TRUE)
 data$cognitive    <- rowMeans(data[c(380:382, 387:388, 393:395)], na.rm=TRUE)
 data$affective    <- rowMeans(data[c(383:384, 389:390, 396:397)], na.rm=TRUE) 
 data$behavioral   <- rowMeans(data[c(385:386, 391:392, 398:399)], na.rm=TRUE) 
+
+cor(data[405:410])
+
+###################################################################################
+###################################################################################
+###################################################################################
+library(lavaan)
+
+bifactor <-'
+Absorption = ~Item_1 + Item_3  + Item_5  + Item_8  + Item_10 + Item_11
+Vigor      = ~Item_14 + Item_16 + Item_17 + Item_19 + Item_21 + Item_22
+Dedication = ~ Item_26 + Item_28 + Item_31 + Item_32 + Item_34 + Item_35
+Cognitive  = ~Item_1  + Item_3  + Item_14 + Item_16 + Item_26 + Item_28
+Affective  = ~Item_5 +  Item_8  + Item_17 + Item_19 + Item_31 + Item_32
+Behavioral = ~Item_10 + Item_11 + Item_21 + Item_22 + Item_34 + Item_35
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor      ~~ 0*Affective
+Vigor      ~~ 0*Behavioral
+Vigor      ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+Fit.mod <- lavaan::cfa(bifactor, data = data, missing = "ML", estimator = 'MLR')
+
+# semPlot::semPaths(Fit.mod2, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3",
+#                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0, pastel=FALSE)
+
+semPlot::semPaths(Fit.mod, bifactor = c("Cognitive", "Affective", "Behavioral"), style="lisrel", "std", layout = "tree3", sizeLat=10, rotation = 2, sizeMan=4.5,edge.label.cex=0.75, edge.color="black", asize=2)
+
+
+# into data frames
+fit1.1 <- as.data.frame(fitMeasures(Fit.mod))
+fit1.1$`fitMeasures(Fit.mod)` <- round(fit1.1$`fitMeasures(Fit1.1)`, 2)
+
+fit1.2 <- as.data.frame(fitMeasures(Fit1.2))
+fit1.2$`fitMeasures(Fit1.2)` <- round(fit1.2$`fitMeasures(Fit1.2)`, 2)
+
+fit.mod2 <- as.data.frame(fitMeasures(Fit.mod2))
+fit.mod2$`fitMeasures(Fit.mod2)` <- round(fit.mod2$`fitMeasures(Fit.mod2)`, 2)
+
+indices_att <- as.data.frame(t(fit1.1[c(3,4,23,29,9,10,19),]))        ## Chi-sq, df, RMSEA, SRMR, CFI, TLI, AIC
+indices_sub <- as.data.frame(t(fit1.2[c(3,4,23,29,9,10,19),]))
+indices_bi <- as.data.frame(t(fit.mod2[c(3,4,44,58,17,18,38),]))
